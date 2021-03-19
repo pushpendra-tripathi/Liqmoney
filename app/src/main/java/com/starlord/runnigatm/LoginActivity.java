fedefced8ad92584e32fa07a44079df019b55441
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2021-03-20 02:37:57 +0530

    skeleton of SplashActivity, LoginActivity, and SignUpActivity is finished and ready for test

---
+++ app/src/main/java/com/starlord/runnigatm/LoginActivity.java
@@ -0,0 +1,133 @@
+package com.starlord.runnigatm;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button signInBtn;
+    private TextView signInText, resetText;
+    FirebaseAuth mAuth;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email_singIn);
+        password = findViewById(R.id.password_signIn);
+        signInBtn = findViewById(R.id.signIn);
+        signInText = findViewById(R.id.signUp_tv);
+        resetText = findViewById(R.id.reset_tv);
+
+        signInBtn.setOnClickListener(view -> {
+            String emailValue = email.getText().toString().trim();
+            String passwordValue = password.getText().toString().trim();
+            validateInputs(emailValue, passwordValue);
+            progressDialog.setMessage("Processing...");
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(emailValue, passwordValue).
+                    addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "You signed in successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+                            progressDialog.dismiss();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        });
+
+        resetText.setOnClickListener(view -> resetDialog());
+
+        signInText.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void validateInputs(String emailValue, String passwordValue) {
+        if (TextUtils.isEmpty(emailValue)) {
+            email.setError("Required field");
+            return;
+        }
+        if (TextUtils.isEmpty(passwordValue)) {
+            password.setError("Required field");
+            return;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+    }
+
+    private void resetDialog() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.password_reset_layout, null);
+        final AlertDialog dialog = myDialog.create();
+        dialog.setView(view);
+        dialog.setCancelable(false);
+
+        final EditText email = view.findViewById(R.id.email_reset);
+        Button cancel = view.findViewById(R.id.cancel_reset);
+        Button send = view.findViewById(R.id.send_reset);
+
+        send.setOnClickListener(view1 -> {
+            String emailValue = email.getText().toString().trim();
+            if (TextUtils.isEmpty(emailValue)) {
+                email.setError("Required");
+                return;
+            }
+            mAuth.sendPasswordResetEmail(emailValue).
+                    addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Password reset link has been sent to your registered email id",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+            dialog.dismiss();
+        });
+
+        cancel.setOnClickListener(view12 -> dialog.dismiss());
+
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/com/starlord/runnigatm/SignUpActivity.java
@@ -0,0 +1,135 @@
+package com.starlord.runnigatm;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+    private EditText userName, email, password;
+    private Button signUpBtn;
+    private TextView signUpText;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        progressDialog = new ProgressDialog(this);
+        userName = findViewById(R.id.userName);
+        email = findViewById(R.id.email_singUp);
+        password = findViewById(R.id.password_signUp);
+        signUpBtn = findViewById(R.id.signUp);
+        signUpText = findViewById(R.id.signIn_tv);
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        signUpBtn.setOnClickListener(view -> {
+            final String userNameValue = userName.getText().toString().trim();
+            String emailValue = email.getText().toString().trim();
+            String passwordValue = password.getText().toString().trim();
+
+            if (TextUtils.isEmpty(userNameValue)) {
+                userName.setError("Required field");
+                return;
+            }
+            if (TextUtils.isEmpty(emailValue)) {
+                email.setError("Required field");
+                return;
+            }
+            if (TextUtils.isEmpty(passwordValue)) {
+                password.setError("Required field");
+                return;
+            }
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(emailValue, passwordValue)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            //update DisplayName of the user
+                            sendEmailVerification();
+                            final FirebaseUser user = mAuth.getCurrentUser();
+                            String userID = user.getUid();
+                            Map<String, Object> userDetails = new HashMap<>();
+                            userDetails.put("name", userNameValue);
+                            userDetails.put("email", emailValue);
+                            userDetails.put("phone", "0987654321");
+
+                            DocumentReference documentReference = db.collection("users").document(userID);
+                            documentReference.set(userDetails).addOnSuccessListener(aVoid -> Log.d(TAG, "User profile updated."))
+                                    .addOnFailureListener(e -> Log.d(TAG, "User profile not updated."));
+
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(userNameValue).build();
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(task1 -> {
+                                        if (task1.isSuccessful()) {
+                                            Log.d(TAG, "Display name updated.");
+                                        }
+                                    });
+
+                            //After user registration
+                            final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            Toast.makeText(getApplicationContext(), "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Sign Up failed", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    });
+        });
+
+        signUpText.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void sendEmailVerification() {
+        // Send verification email
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Email sent
+                        Log.d(TAG, "Verification email sent");
+                    }
+                });
+    }
+}
